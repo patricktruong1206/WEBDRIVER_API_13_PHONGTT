a30b2e9e2b9434bbd27a8db152f6e69aed980179
Truong Tuan Phong <phongtruong1206@gmail.com>
2020-01-08 14:14:09 +0700

    Update testcase topic Iframe

--- src/webdriver_api/Topic_08_PopUp_Frame_Iframe.java
+++ src/webdriver_api/Topic_08_PopUp_Frame_Iframe.java
@@ -5,9 +5,14 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -16,13 +21,27 @@ import org.testng.annotations.Test;
 public class Topic_08_PopUp_Frame_Iframe {
 	WebDriver driver;
 	By popupBanner= By.xpath("//img[contains(@class,'popupbanner')]");
+	WebDriverWait waitExplicit;
+	WebElement element;
+	JavascriptExecutor jsExecutor;
+	String projectPath= System.getProperty("user.dir");
 
 	@BeforeClass
 	public void beforeClass() {
+		
+		//System.setProperty("webdriver.chrome.driver", projectPath+ "\\libraries\\chromedriver.exe");
+		//ChromeOptions options = new ChromeOptions();
+		//options.addArguments("--lang=vi");
+		//driver = new ChromeDriver(options);
+		
+		
 		driver = new FirefoxDriver();
+		jsExecutor =  (JavascriptExecutor) driver;
+		
+		waitExplicit = new WebDriverWait(driver,5);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
+	
 	}
 
 	@Test
@@ -71,7 +90,59 @@ public class Topic_08_PopUp_Frame_Iframe {
 		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='Privacy Policy']")).isDisplayed());
 			
 	}
-
+	
+	@Test
+	public void TC_01_New_HandlingPopUpIFrame() throws InterruptedException {
+		driver.get("https://kyna.vn/");
+		
+		List<WebElement> fancyPopup= driver.findElements(By.xpath("//div[@class='fancybox-inner']"));
+		
+		if(fancyPopup.size() >0) {
+			Assert.assertTrue(fancyPopup.get(0).isDisplayed());
+			driver.findElement(By.cssSelector(".fancybox-close")).click();
+		}
+		
+		Thread.sleep(3000);
+		
+		driver.switchTo().frame(driver.findElement(By.xpath("//div[@class='fanpage']//iframe")));
+		
+		
+		boolean facebookIframe = driver.findElement(By.cssSelector("#facebook")).isDisplayed();
+		System.out.println("Facebook iframe is displayed" +facebookIframe);
+		Assert.assertTrue(facebookIframe);
+		
+		
+		
+		String  facebookLikes= driver.findElement(By.xpath("//a[text()='Kyna.vn']/parent::div/following-sibling::div")).getText();
+		System.out.println("Facebook like is displayed" +facebookLikes);
+		Assert.assertEquals(facebookLikes,"170K likes");
+		
+		driver.switchTo().parentFrame();
+		
+		waitExplicit.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//li//a[@class='button-login']")));
+		
+		driver.findElement(By.xpath("//li//a[@class='button-login']")).click();
+		
+		driver.findElement(By.xpath("//input[@id='user-login']")).sendKeys("automationfc.vn@gmail.com");
+		
+		driver.findElement(By.xpath("//input[@id='user-password']")).sendKeys("automationfc.vn@gmail.com");
+		
+		Thread.sleep(3000);
+		
+		waitExplicit.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='btn-submit-login']")));
+				
+		driver.findElement(By.xpath("//button[@id='btn-submit-login']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//span[text()='Automation FC']")).isDisplayed());
+		
+	}
+	
+ 
+	
+	
+	
+	
+	
 	@Test
 	public void TC_02_HandlingWindowstab() throws InterruptedException  {
 		
@@ -120,7 +191,7 @@ public class Topic_08_PopUp_Frame_Iframe {
 		Assert.assertTrue(closeAllWindowsWithoutParent(parentWindowId));
 	}
 
-	@Test
+	/*@Test
 	public void TC_03_HandlingWindows_Tab_Frame() {
 		driver.get("http://www.hdfcbank.com/");
 
@@ -133,7 +204,84 @@ public class Topic_08_PopUp_Frame_Iframe {
 		//This test case is out-dated. To be updated later
 			
 	}
-
+	*/
+	@Test
+	public void TC_03_New_HandlingWindows_Tab() throws InterruptedException{
+		driver.get("https://kyna.vn/");
+		
+		String parentWindowId= driver.getWindowHandle();
+		
+		List<WebElement> fancyPopup= driver.findElements(By.xpath("//div[@class='fancybox-inner']"));
+		
+		if(fancyPopup.size() >0) {
+			Assert.assertTrue(fancyPopup.get(0).isDisplayed());
+			driver.findElement(By.cssSelector(".fancybox-close")).click();
+		}
+		
+		Thread.sleep(3000);
+		
+		
+		clickToElementByJS("//div[@class='hotline']//a//img[@alt='facebook']");
+		
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		for (String id: allWindowIDs) {
+			System.out.println("ID" +id);
+			
+			if(!id.equals(parentWindowId)) {
+				driver.switchTo().window(id);
+			}
+		}
+		
+		switchToChildWindowByTitle("Kyna.vn - Trang chủ | Facebook");
+		
+		Assert.assertEquals(driver.getTitle(), "Kyna.vn - Trang chủ | Facebook");
+		
+		
+		driver.switchTo().window(parentWindowId);
+			
+		clickToElementByJS("//div[@class='hotline']//a//img[@alt='youtube']");
+		
+		switchToChildWindowByTitle("Kyna.vn - YouTube");
+		
+		Assert.assertEquals(driver.getTitle(), "Kyna.vn - YouTube");
+		
+		driver.switchTo().window(parentWindowId);
+		
+		clickToElementByJS("//div[@class='hotline']//a//img[@alt='zalo']");
+		switchToChildWindowByTitle("Kyna.vn");
+		
+		Assert.assertEquals(driver.getTitle(), "Kyna.vn");
+		
+		driver.switchTo().window(parentWindowId);
+		
+		//clickToElementByJS("//div[@class='app-col']//a//img[@alt='apple-app-icon']");
+		
+		//switchToChildWindowByTitle("‎KYNA on the App Store");
+		
+		//Assert.assertEquals(driver.getTitle(), "‎KYNA on the App Store");
+		
+		driver.switchTo().window(parentWindowId);
+		
+		clickToElementByJS("//div[@class='app-col']//a//img[@alt='android-app-icon']");
+		
+		switchToChildWindowByTitle("KYNA - Học online cùng chuyên gia - Apps on Google Play");
+		
+		Assert.assertEquals(driver.getTitle(), "KYNA - Học online cùng chuyên gia - Apps on Google Play");
+		
+		driver.switchTo().window(parentWindowId);
+		
+		driver.switchTo().frame(driver.findElement(By.xpath("//div[@class='fanpage']//iframe")));
+		
+		clickToElementByJS("//a[@title='Kyna.vn']");
+		
+		switchToChildWindowByTitle("Kyna.vn - Trang chủ | Facebook");
+		
+		Assert.assertEquals(driver.getTitle(), "Kyna.vn - Trang chủ | Facebook");
+			
+		closeAllWindowsWithoutParent(parentWindowId);
+		
+	}
+	
 	@Test
 	public void TC_04_HandlingWindows() {
 		//Step 1 Access page
@@ -156,7 +304,7 @@ public class Topic_08_PopUp_Frame_Iframe {
 		
 		driver.findElement(By.xpath("//a[@title='Samsung Galaxy']//following-sibling::div//descendant::a[text()='Add to Compare'] ")).click();
 		
-					//Verify text
+		//Verify text
 		
 		Assert.assertEquals(driver.findElement(By.xpath("//span[text()='The product Samsung Galaxy has been added to comparison list.']")).getText(), "The product Samsung Galaxy has been added to comparison list.");
 		//Step 5 Click to compare button
@@ -244,9 +392,14 @@ public class Topic_08_PopUp_Frame_Iframe {
 			}
 	}
 	
+	public void clickToElementByJS(String locator) {
+		element= driver.findElement(By.xpath(locator));
+		jsExecutor.executeScript("arguments[0].click();", element);
+	}
+	
 	@AfterClass
 	public void afterClass() {
-		driver.quit();
+		//driver.quit();
 	}
 
 }
